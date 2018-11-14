929ee30e3f6878ddf6ba34c27e6d1dd3f7bd916a
pohl989 <pohl989@gmail.com>
2018-11-14 09:22:06 -0700

    testing keys

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Header } from 'semantic-ui-react';
+import {
+    Link
+} from 'react-router-dom';
+import {
+    Header
+} from 'semantic-ui-react';
 
-const NoMatch = () => (
- <Header as="h3" textAlign="center">
-   Page not found return
-   <Link to="/"> Home</Link>
- </Header>
+const NoMatch = () => ( <
+    Header as = "h3"
+    textAlign = "center" >
+    Page not found
+    return
+
+    <Link to = "/" > Home < /Link> <
+    /Header>
 )
 
 export default NoMatch;
